b7f9d5c8e772744632e5f002a4507b01eda3b589
Rafael <Rafael@DESKTOP-I4KUQ0M>
2021-04-19 23:22:16 -0300

    Teste Commit BitBucket

--- src/conexao/ConnectionFactory.java
+++ src/conexao/ConnectionFactory.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package conexao;
 
 import java.sql.Connection;
@@ -82,17 +77,4 @@ public class ConnectionFactory {
         }
     }
 
-    //CONFIRMAR SE NÃO PRECISA E DEPOIS TIRAR
-//        /**
-//     * Obtém uma conexão para a base de dados sakila.
-//     *
-//     * @return Uma conexão para a base de dados sakila.
-//     * @throws SQLException Caso ocorra algum problema durante a conexão.
-//     */
-//    public static Connection getSakilaConnection() throws SQLException {
-// 
-//        return getConnection();
-// 
-//    }
-    
 }
--- src/controller/TelaAgendamentosController.java
+++ src/controller/TelaAgendamentosController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import conexao.ConnectionFactory;
@@ -22,10 +17,6 @@ import util.Data;
 import view.TelaAgendamentos;
 import view.TelaPrincipal;
 
-/**
- *
- * @author Rafael
- */
 public class TelaAgendamentosController {
         
     private TelaAgendamentos view;
